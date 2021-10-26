7f5ee9494f9f3ef5f0ff0b802f50816bfe6c85a4
galcober <gabriel.alcober@gmail.com>
2021-10-26 21:14:07 +0200

    Updated RestController

--- src/main/java/com/manage/home/controller/PayrollController.java
+++ src/main/java/com/manage/home/controller/PayrollController.java
@@ -1,18 +1,18 @@
 package com.manage.home.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.manage.home.model.Payroll;
 import com.manage.home.repository.PayrollRepository;
 
-@Controller
+@RestController
 @RequestMapping(path="/payroll")
 public class PayrollController {
 	
@@ -20,7 +20,7 @@ public class PayrollController {
 	private PayrollRepository payrollRepository;
 	
 	@PostMapping(path="/add")
-	public @ResponseBody String add (
+	public String add (
 			@RequestParam int month, int year, String pdf) {
 		
 		Payroll payroll = new Payroll();
@@ -33,12 +33,12 @@ public class PayrollController {
 	}
 	
 	@GetMapping(path="/all")
-	public @ResponseBody Iterable<Payroll> getAll() {
+	public Iterable<Payroll> getAll() {
 		return payrollRepository.findAll();
 	}
 	
 	@GetMapping(path="/pdf/{month}/{year}")
-	public @ResponseBody String getPdfByMonthAndYear(@PathVariable int month, @PathVariable int year) {
+	public String getPdfByMonthAndYear(@PathVariable int month, @PathVariable int year) {
 		String pdfBase64 = payrollRepository.findByMonthAndYear(month, year);
 //		return pdfBase64;
 //		return "<embed src=\"data:application/pdf;base64,"+pdfBase64+" \" type=\"application/pdf\" width=\"100%\" height=\"600px\"/>";
